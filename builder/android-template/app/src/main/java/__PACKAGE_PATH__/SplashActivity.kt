b71345d637bca8b1bf06950e6631c13e554bc373
devil7079gamer-design <devil7079gamer@gmail.com>
2026-07-23 19:32:30 +0500

    Add import statement for app resources in SplashActivity

--- builder/android-template/app/src/main/java/__PACKAGE_PATH__/SplashActivity.kt
+++ builder/android-template/app/src/main/java/__PACKAGE_PATH__/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.yesyeyes.app.R
 import androidx.appcompat.app.AppCompatActivity
 
 
