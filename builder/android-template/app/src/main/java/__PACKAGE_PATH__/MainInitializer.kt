8318fecefdb6b1e915daf99e50d6081c68228ba3
devil7079gamer-design <devil7079gamer@gmail.com>
2026-07-23 19:30:43 +0500

    Update MainInitializer.kt

--- builder/android-template/app/src/main/java/__PACKAGE_PATH__/MainInitializer.kt
+++ builder/android-template/app/src/main/java/__PACKAGE_PATH__/MainInitializer.kt
@@ -1,29 +1,9 @@
 import android.app.Application
 
-
 class MainInitializer : Application() {
 
-
     override fun onCreate() {
-
         super.onCreate()
-
-
-        // Initialize notifications
-
-        NotificationHelper(
-            this
-        ).createChannel()
-
-
-        // Load ads
-
-        if (ReleaseConfig.ENABLE_ADS) {
-
-            // Ads will load from activity
-
-        }
-
     }
 
 }
