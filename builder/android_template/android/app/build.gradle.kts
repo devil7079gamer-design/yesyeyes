85c38b13fdf6b5bd393ab8d3cf13277bb45694d1
devil7079gamer-design <devil7079gamer@gmail.com>
2026-07-29 22:08:47 +0500

    Update build.gradle.kts

--- builder/android_template/android/app/build.gradle.kts
+++ builder/android_template/android/app/build.gradle.kts
@@ -6,25 +6,24 @@ plugins {
 }
 
 android {
-    namespace = "com.example.statefulclickcounter"
+    namespace = "com.example.vex_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        // 🚀 Enable Core Library Desugaring for Notifications
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.statefulclickcounter"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        applicationId = "com.example.vex_app"
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -32,8 +31,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +39,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // 🚀 Core Library Desugaring Support (Kotlin Syntax)
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
